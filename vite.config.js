4a856fa0b1131a2f1da5b9f13671cb331ea6cf67
Peter Tran <peter.tran@associa.us>
2026-09-06 14:19:28 -0500

    Setup Vite React Project and Github Actions flow

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/peters-better-weather-forecaster/', // MUST match your exact GitHub repository name})
+})
